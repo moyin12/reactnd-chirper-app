bdf93e95d8a92888881675417b084cdc61d1f2b2
Moyinoluwa Boluwatife Ariyo <moyin.ariyo@gmail.com>
2020-05-23 23:36:28 +0100

    Added a loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,22 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveTweets } from '../actions/tweets'
 import { setAuthedUser } from '../actions/authedUser'
+import {
+    showLoading,
+    hideLoading
+} from 'react-redux-loading'
 
 const AUTHED_ID = 'moyinariyo'
 
 export const handleInitialData = () => {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
         .then(({ users, tweets }) => {
             dispatch(receiveUsers(users))
             dispatch(receiveTweets(tweets))
             dispatch(setAuthedUser(AUTHED_ID))
+            dispatch(hideLoading())
         })
     }
 }
